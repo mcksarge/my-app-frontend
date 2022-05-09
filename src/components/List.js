fc521ac7f7c83606d26ec4ce4a13a24dfdbb4d20
Fred <higgins.fred.s@gmail.com>
2022-05-09 12:55:38 -0700

    initial commit

---
+++ src/components/List.js
@@ -0,0 +1,14 @@
+import ToDo from './ToDo'
+
+function List(){
+    return(
+        <div id="list-container">
+            <h2 class="list-title">Today</h2>
+            <ToDo />
+        </div>
+
+        
+    )
+}
+
+export default List;
---
+++ src/components/ToDo.js
@@ -0,0 +1,13 @@
+import Task from './Task'
+
+function ToDo(){
+    return (
+        <div>
+            <ul>
+                <Task />
+            </ul>
+        </div>
+    )
+}
+
+export default ToDo;
