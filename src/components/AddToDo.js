9175b76f12fc9e1da893815849fd2eed474f5e1c
Fred <higgins.fred.s@gmail.com>
2022-05-11 15:21:32 -0700

    Add component for adding new tasks, import to List component

--- src/components/AddToDo.js
+++ src/components/AddToDo.js
@@ -4,7 +4,7 @@ function AddToDo() {
     return (
         <div id="add-todo">
             <form>
-                <label for="new-task">Add a New Task:</label>
+                <label id="new-task-title" for="new-task"><strong>Add a New Task:</strong></label>
                 <input id="new-task" type="text" value="Enter a task..."></input>
                 <input id="submit-task" type="submit" value="Submit"></input>
             </form>
