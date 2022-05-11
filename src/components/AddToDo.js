30ee4bdbf49b1f4be21dc02db76e5ec83578cbc5
Fred <higgins.fred.s@gmail.com>
2022-05-11 15:39:48 -0700

    Add callback functions for change and submit functions in AddToDo

--- src/components/AddToDo.js
+++ src/components/AddToDo.js
@@ -1,12 +1,22 @@
-
+import {useState} from "react";
 
 function AddToDo() {
+    const [newToDo, setNewToDo] = useState("")
+
+    function handleSubmit(e){
+        e.preventDefault();
+    }
+
+    function handleChange(e) {
+        setNewToDo(e.target.value)
+    }
+
     return (
         <div id="add-todo">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label id="new-task-title" for="new-task"><strong>Add a New Task:</strong></label>
-                <input id="new-task" type="text" value="Enter a task..."></input>
-                <input id="submit-task" type="submit" value="Submit"></input>
+                <input id="new-task" type="text" placeholder="Enter a task..." value={newToDo} onChange={handleChange}></input>
+                <input id="submit-task" type="submit" value="Submit" ></input>
             </form>
         </div>
     )
