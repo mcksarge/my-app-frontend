5db1258f30c4061d796f57e7a23485036a7b4bc0
Fred <higgins.fred.s@gmail.com>
2022-05-11 18:13:34 -0700

    End of day save

--- src/components/Task.js
+++ src/components/Task.js
@@ -3,10 +3,14 @@
 function Item(){
     let array = ["Do chores", "Pick up mail", "Wash Dishes", "Clean car", "Ice my feet"]
 
+    function handleComplete(e){
+        console.log("You completed me!")
+    }
+
     let list = array.map((task) =>
         <div>
             <li>{task}</li>
-            <button class="complete-btn">Complete</button>
+            <button class="complete-btn" onClick={handleComplete}>Complete</button>
             <button class="delete-btn">Delete</button>
             <hr></hr>
         </div> 
